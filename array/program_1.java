aced746352fa16a0c335518fb67ad5eb5ab01ccf
Bhavesh Patil <bhavesh.22020176@viit.ac.in>
2021-12-01 21:52:01 +0530

    First Commit 01_12

---
+++ array/program_1.java
@@ -0,0 +1,37 @@
+package array;
+
+public class program_1 {
+    //this method print array
+    static void printArray(int arr[], int arrLength){
+        
+        for(int i = 0 ; i < arrLength ; i++){
+            System.out.print(" "+arr[i]);
+        }
+        System.out.println();
+    }
+
+    // this method reverse the array
+    static void reverseArray(int arr[], int arrStart, int arrEnd){
+        int temp;
+
+        while(arrStart < arrEnd){
+            temp = arr[arrStart];
+            arr[arrStart] = arr[arrEnd];
+            arr[arrEnd] = temp;
+
+            arrStart++;
+            arrEnd--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 4, 5, 6, 7};
+
+        printArray(arr, arr.length);
+
+        reverseArray(arr, 0, arr.length-1);
+
+        System.out.println("Reverse array is :");
+
+        printArray(arr, arr.length);
+    }
+}
---
+++ array/program_2.java
@@ -0,0 +1,43 @@
+package array;
+
+public class program_2 {
+    static class MinMax{
+        int min;
+        int max;
+    }
+
+    static MinMax getMinMax(int arr[], int arrSize){
+        MinMax minMax = new MinMax();
+
+        if(arrSize == 1){
+            minMax.max = arr[0];
+            minMax.min = arr[0];
+            return minMax;
+        }
+
+        if(arr[0] > arr[1]){
+            minMax.max = arr[0];
+            minMax.min = arr[1];
+        }else{
+            minMax.max = arr[1];
+            minMax.min = arr[0];
+        }
+
+        for(int i = 2 ; i < arrSize ; i++){
+            if(arr[i] > minMax.max)
+                minMax.max = arr[i];
+            else if(arr[i] < minMax.min)
+                minMax.min = arr[i];
+        }
+        return minMax;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {200, 300, 800, 10000, 1, 40};
+        MinMax minmax = getMinMax(arr, arr.length);
+        // printf() method is used to print string along with int, double, char, float etc., variable
+        System.out.printf("\nMinimum element is %d", minmax.min);
+        System.out.printf("\nMaximum element is %d", minmax.max);
+
+    }
+}
