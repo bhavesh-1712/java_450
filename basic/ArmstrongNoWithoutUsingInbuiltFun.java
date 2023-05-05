979c7f8ad555d527fa6387936bf2afc94c935505
Bhavesh Patil <89469343+bhavesh-1712@users.noreply.github.com>
2023-05-05 13:15:51 +0530

    Added : Comment What's Armstrong number

--- basic/ArmstrongNoWithoutUsingInbuiltFun.java
+++ basic/ArmstrongNoWithoutUsingInbuiltFun.java
@@ -50,6 +50,7 @@ public class ArmstrongNoWithoutUsingInbuiltFun {
     }
 }
 /**
+ * Armstrong Number : Armstrong number is a number that is equal to the sum of cubes of its digits.
  * OUTPUT 
  * 
  * 121
